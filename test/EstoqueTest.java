f8f316f72449cad3bbba852bcaeaef44d6ceb11e
mandypry <amanda_sucesso@hotmail.com>
2019-09-02 14:35:42 -0300

    Refatorando as classses EstoqueTest e Estoque

--- test/EstoqueTest.java
+++ test/EstoqueTest.java
@@ -6,11 +6,10 @@ public class EstoqueTest{
 
     @Test
     public void CriarProdutoEstoque() {
+            Estoque estoque = new Estoque();
+
             Produto produto = new Produto(); //novo produto da classe "Produto"
             produto.setNome("Suco de Frutas Vermelhas");
-
-            Estoque estoque = new Estoque();
-            estoque.produto = produto;
 
             Categoria categoria = new Categoria(); //novo produto da classe "Categoria"
             categoria.setTipo("Alimenticio");
@@ -18,19 +17,17 @@ public class EstoqueTest{
             Subcategoria subcategoria = new Subcategoria(); //novo produto da classe "Subcategoria"
             subcategoria.setUnidMedida("Liquido");
 
-            estoque.quantidade = 5;
+            estoque.setQuantidade(5);
 
             //Testando o "set" do produto {Set modifica o atributo}
             estoque.setProduto(produto);
             estoque.setCategoria(categoria);
             estoque.setSubcategoria(subcategoria);
-            Integer quantidade = 5;
-            estoque.setQuantidade(quantidade);
 
             //Testando o "get" do produto {Get acessa o atributo 'pega'}
             assertThat(estoque.getProduto().getNome(), is("Suco de Frutas Vermelhas"));
-            assertThat(estoque.getCategoria(), is("Alimenticio"));
-            assertThat(estoque.getSubcategoria(), is("Liquido"));
+            assertThat(estoque.getCategoria().getTipo(), is("Alimenticio"));
+            assertThat(estoque.getSubcategoria().getUnidMedida(), is("Liquido"));
             assertThat(estoque.getQuantidade(), is(5));
         }
     }
